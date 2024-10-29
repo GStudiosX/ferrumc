c7573369a9ca7175145ad7891296d5f1aece93e8
GStudiosX2 <76548041+GStudiosX2@users.noreply.github.com>
2024-10-29 15:49:12 +0000

    Fix keep alive system making shutdown longer

--- src/bin/src/systems/keep_alive_system.rs
+++ src/bin/src/systems/keep_alive_system.rs
@@ -1,20 +1,21 @@
 use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
+//use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Duration;
 use async_trait::async_trait;
 use tracing::info;
 use ferrumc_core::identity::player_identity::PlayerIdentity;
 use ferrumc_net::GlobalState;
 use crate::systems::definition::System;
+use tokio::sync::Notify;
 
 pub struct KeepAliveSystem {
-    shutdown: AtomicBool
+    shutdown: Notify
 }
 
 impl KeepAliveSystem {
-    pub const fn new() -> Self {
+    pub fn new() -> Self {
         Self {
-            shutdown: AtomicBool::new(false)
+            shutdown: Notify::new()
         }
     }
 }
@@ -22,17 +23,22 @@ impl KeepAliveSystem {
 #[async_trait]
 impl System for KeepAliveSystem {
     async fn start(self: Arc<Self>, state: GlobalState) {
-        while !self.shutdown.load(Ordering::Relaxed) {
-            let online_players = state.universe.query::<&PlayerIdentity>();
-            info!("Online players: {}", online_players.count());
+        tokio::select! {
+            _ = async move {
+                loop {
+                    let online_players = state.universe.query::<&PlayerIdentity>();
+                    info!("Online players: {}", online_players.count());
 
-            tokio::time::sleep(Duration::from_secs(5)).await;
+                    tokio::time::sleep(Duration::from_secs(5)).await;
+                }
+            } => {},
+            _ = self.shutdown.notified() => {}
         }
     }
 
     async fn stop(self: Arc<Self>, _state: GlobalState) {
         tracing::debug!("Stopping keep alive system...");
-        self.shutdown.store(true, Ordering::Relaxed);
+        self.shutdown.notify_one();
     }
 
     fn name(&self) -> &'static str {
